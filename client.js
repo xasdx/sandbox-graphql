27035e421d2ea83af34f8c90b6a8ae18a9c5e32c
_asdx_ <xasdx@tuta.io>
2017-12-21 18:14:05 +0000

    Add a client to query the gql api

---
+++ client.js
@@ -0,0 +1,18 @@
+require("es6-promise").polyfill()
+require("isomorphic-fetch")
+
+let url = `http://localhost:${process.env.PORT}/graphql`
+
+let headers = new Headers()
+headers.append("Content-Type", "application/json")
+headers.append("Accept", "application/json")
+
+let requestBody = { query: "{ hello }" }
+
+let options = {
+  method: "POST",
+  body: JSON.stringify(requestBody),
+  headers
+}
+
+fetch(url, options).then(res => res.json()).then(console.log)
