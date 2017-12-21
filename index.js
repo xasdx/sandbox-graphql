25e0a9396f6d4f7dc01cbf5120f56c874b286a3c
_asdx_ <xasdx@tuta.io>
2017-12-21 17:50:22 +0000

    Set up express integrated with gql

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-let { graphql, buildSchema } = require("graphql")
+let express = require("express")
+let gqlHttp = require("express-graphql")
+let { buildSchema } = require("graphql")
 
 let schema = buildSchema(`
   type Query {
@@ -10,4 +12,13 @@ let root = {
   hello: () => "hello world"
 }
 
-graphql(schema, "{ hello }", root).then(console.log)
+let app = express()
+
+app.use("/graphql", gqlHttp({
+  schema: schema,
+  rootValue: root,
+  graphiql: true
+}))
+
+app.listen(process.env.PORT, process.env.IP)
+console.log("up")
